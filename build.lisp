357302d7bd7ef3c0daa5ba2bed6ebb20ba189db5
Keith Johnson <kjcjohnson@ymail.com>
2022-11-06 17:42:46 -0600

    Initial cli app structure

--- build.lisp
+++ build.lisp
@@ -2,5 +2,11 @@
 ;;;; Build.lisp - build the application
 ;;;;
 ;;; Assumption: ASDF and quicklisp are already loaded
+
+;;; Make sure all the dependencies are downloaded
 (ql:quickload "com.kjcjohnson.ks2.runner")
+
+;;; Then compile everything and dump an image
+(asdf:oos 'asdf:program-op "com.kjcjohnson.ks2.runner")
+
 
